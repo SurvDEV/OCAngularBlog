00764289db02496bca237e839475917e70155003
Surv <mathieufares.dev@gmail.com>
2019-02-09 18:59:38 +0100

    Angular Activité 1 posts like/dislike

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ocangularblog';
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: '1 Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+      loveIts: 0,
+      created_at: Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: '2 Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+      loveIts: 0,
+      created_at: Date()
+    },
+    {
+      title: 'Mon troisième post',
+      content: '3 Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s Lorem Ipsum Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+      loveIts: 0,
+      created_at: Date()
+    }
+  ];
 }
